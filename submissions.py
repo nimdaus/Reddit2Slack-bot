5813888abfa0522dc444c2078003fb0795975515
nimdaus <30591465+nimdaus@users.noreply.github.com>
2020-10-21 09:07:58 -0400

    Update submissions.py
    correct

--- submissions.py
+++ submissions.py
@@ -33,7 +33,7 @@ while True:
             if query in normalized_title:
                 submission.created_utc_format = datetime.fromtimestamp(submission.created_utc, tz=timezone.utc).isoformat()
                 print("Created: {}, Title: {}".format(submission.created_utc_format, submission.title))
-                compound_score = analyzer.polarity.scores(submission.selftext)["compound"]
+                compound_score = analyzer.polarity_scores(submission.selftext)["compound"]
                 if compound_score > 0.5:
                     sentiment_outcome = ":slightly_smiling_face: Seems *Positive*"
                     client = WebClient(token=slack_token)
