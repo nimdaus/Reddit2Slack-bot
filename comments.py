6030830502a066c423d6210a1940def7c2025664
nimdaus <joshuaplambert@gmail.com>
2021-02-04 23:34:18 -0500

    Healthchecks, function-ise, slack-sdk

--- comments.py
+++ comments.py
@@ -5,81 +5,58 @@ import json
 from datetime import datetime, timezone
 import requests
 import time
-from slack import WebClient
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 #import os
 
-def slack_comment():
-    print(f"Comment on: *<https://reddit.com{comment.permalink}|{comment.submission.title}>* by <https://reddit.com/u/{comment.author.name}|{comment.author.name}>\n*Posted*: <!date^{round(comment.created_utc)}^{{date_short_pretty}} {{time}}|{comment.created_utc_format}>")
-    # client = WebClient(token=slack_token)
-    # client.chat_postMessage(
-    # channel = slack_channel,
-    # blocks = [
-    #     {
-    #         "type": "section",
-    #         "text": {
-    #             "type": "mrkdwn",
-    #             "text": f"Comment on: *<https://reddit.com{comment.permalink}|{comment.submission.title}>* by <https://reddit.com/u/{comment.author.name}|{comment.author.name}>\n*Posted*: <!date^{round(comment.created_utc)}^{{date_short_pretty}} {{time}}|{comment.created_utc_format}>"
-    #         }
-    #         },
-    #         {
-    #             "type": "divider"
-    #         },
-    #         {
-    #             "type": "section",
-    #             "text": {
-    #                 "type": "mrkdwn",
-    #                 "text": f"{comment.body_short}..."
-    #             }
-    #         },
-    #         {
-    #             "type": "context",
-    #             "elements": [
-    #                 {
-    #                 "type": "mrkdwn",
-    #                 "text": f"{sentiment_outcome}"
-    #                 }
-    #             ]
-    #         }
-    #     ]
-    # )
+def slack_comment(permalink, title, name, created_utc, created_utc_format, body_short, sentiment_outcome):
+    print(f"Comment on: *<https://reddit.com{permalink}|{title}>* by <https://reddit.com/u/{name}|{name}>\n*Posted*: <!date^{round(created_utc)}^{{date_short_pretty}} {{time}}|{created_utc_format}>")
+    client = WebClient(token=slack_token)
+    message = json.load(open("msg_template.json", "r"))
+    message[0]["text"]["text"] = f"Comment on: *<https://reddit.com{permalink}|{title}>* by <https://reddit.com/u/{name}|{name}>\n*Posted*: <!date^{round(created_utc)}^{{date_short_pretty}} {{time}}|{created_utc_format}>"
+    message[2]["text"]["text"] = f"{body_short}..."
+    message[3]["elements"]["text"] = f"{sentiment_outcome}"
+    client.chat_postMessage(channel = slack_channel, blocks = message)
     return
 
-def heartbeat():
-    try:
-        requests.get("https://hc-ping.com/42bb23fb-68fb-4b7a-b933-fda65e30e5fe", timeout=10)
-    except requests.RequestException as e:
-        # Log ping failure here...
-        print("Ping failed: %s" % e)
+def heartbeat(good_state, info):
+    if good_state == true:
+        requests.get(f"https://hc-ping.com/{config['healthcheck']['uuid']}", timeout=10)
+    else:
+        requests.post(f"https://hc-ping.com/{config['healthcheck']['uuid']}/fail", data=info)
+    return
 
-config = json.load(open('config.json'))
-analyzer = SentimentIntensityAnalyzer()
+def load_config():
+    ## FOR WHEN DOCKERIZED
+    #os.environ['client-id']
+    #os.environ['client-secret']
+    #os.environ['username']
+    #os.environ['password']
+    #os.environ['user-agent']
+    #os.environ['query']
+    #os.environ['subreddit']
+    #os.environ['token']
+    #os.environ['channel']
+    global reddit, query, subreddit_name, slack_token, slack_channel
+    config = json.load(open('config.json'))
+    reddit = praw.Reddit(
+        client_id=config['reddit']['client-id'],
+        client_secret=config['reddit']['client-secret'],
+        username=config['reddit']['username'],
+        password=config['reddit']['password'],
+        user_agent=config['reddit']['user_agent'])
 
-## FOR WHEN DOCKERIZED
-#os.environ['client-id']
-#os.environ['client-secret']
-#os.environ['username']
-#os.environ['password']
-#os.environ['user-agent']
-#os.environ['query']
-#os.environ['subreddit']
-#os.environ['token']
-#os.environ['channel']
-
-reddit = praw.Reddit(
-    client_id=config['reddit']['client-id'],
-    client_secret=config['reddit']['client-secret'],
-    username=config['reddit']['username'],
-    password=config['reddit']['password'],
-    user_agent=config['reddit']['user_agent'])
-
-query = config['reddit']['query']
-subreddit_name = config['reddit']['subreddit']
-slack_token = config['slack']['token']
-slack_channel = config['slack']['channel']
+    query = config['reddit']['query']
+    subreddit_name = config['reddit']['subreddit']
+    slack_token = config['slack']['token']
+    slack_channel = config['slack']['channel']
+    return reddit, query, subreddit_name, slack_token, slack_channel
 
 while True:
     try:
+        load_config()
+        analyzer = SentimentIntensityAnalyzer()
         for comment in reddit.subreddit(subreddit_name).stream.comments(pause_after=1, skip_existing=True):
             if comment is None:
                 continue
@@ -91,19 +68,33 @@ while True:
                 compound_score = analyzer.polarity_scores(comment.body)["compound"]
                 if compound_score > 0.5:
                     sentiment_outcome = ":slightly_smiling_face: Seems Positive"
-                    slack_comment()
+                    slack_comment(permalink=comment.permalink, title=comment.submission.title, name=comment.author.name, created_utc=comment.created_utc, created_utc_format=comment.created_utc_format, body_short=comment.body_short, sentiment_outcome=sentiment_outcome)
+                    heartbeat(good_state=True, info="Success")
                 elif compound_score < 0:
                     sentiment_outcome = ":disappointed: Seems *Negative*"
-                    slack_comment()
+                    slack_comment(permalink=comment.permalink, title=comment.submission.title, name=comment.author.name, created_utc=comment.created_utc, created_utc_format=comment.created_utc_format, body_short=comment.body_short, sentiment_outcome=sentiment_outcome)
+                    heartbeat(good_state=True, info="Success")
                 else:
                     sentiment_outcome = "¯\_(ツ)_/¯ Sorry I couldn't detect a substantial sentiment"
-                    slack_comment()
-    except RedditAPIException as exception:
-        print("error - waiting 5 seconds and trying again")
+                    slack_comment(permalink=comment.permalink, title=comment.submission.title, name=comment.author.name, created_utc=comment.created_utc, created_utc_format=comment.created_utc_format, body_short=comment.body_short, sentiment_outcome=sentiment_outcome)
+                    heartbeat(good_state=True, info="Success")
+    except RedditAPIException as e:
+        print(f"ErrorMSG - {e}\nWaiting 5 seconds and trying again")
+        heartbeat(good_state=False, info=f"{e}")
         time.sleep(5)
         continue
     except PrawcoreException as e:
-        print("error - waiting 5 seconds and trying again")
+        print(f"ErrorMSG - {e}\nWaiting 5 seconds and trying again")
+        heartbeat(good_state=False, info=f"{e}")
         time.sleep(5)
         continue
-    
+    except SlackApiError as e:
+        print(f"ErrorMSG - {e}\nWaiting 5 seconds and trying again")
+        heartbeat(good_state=False, info=f"{e}")
+        time.sleep(5)
+        continue
+    except Exception as e:
+        print(f"ErrorMSG - {e}\nWaiting 5 seconds and trying again")
+        heartbeat(good_state=False, info=f"{e}")
+        time.sleep(5)
+        quit()
